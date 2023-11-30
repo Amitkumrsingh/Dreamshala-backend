cddbb0140f79b7bea75e13f3466024ee786c1c33
Amitkumrsingh <aksingh1109@gmail.com>
2023-11-30 16:25:46 +0530

    added rds

--- dreamshala/settings.py
+++ dreamshala/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'dreamshala.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dreamshala-backend',
+        'USER': 'dreamshala',
+        'PASSWORD': 'Dreamshala#2023',
+        'HOST': 'dreamshala-backend.c3ypzskok0a2.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
