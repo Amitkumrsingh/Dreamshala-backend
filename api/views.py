b7b01ba031eb933e845a91bc74d8d4d0a4981768
Amitkumrsingh <aksingh1109@gmail.com>
2023-12-01 10:30:28 +0530

    Added static files

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import CustomUser
 from .serializers import UserSerializer
 
+
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
