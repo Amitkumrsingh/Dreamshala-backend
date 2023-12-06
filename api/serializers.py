fcac3add789b4641a4aa27923995549af5a3bb18
Amitkumrsingh <aksingh1109@gmail.com>
2023-12-06 14:42:41 +0530

    Add RestFramework

--- api/serializers.py
+++ api/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user_type = validated_data.pop('user_type', 'ST')  # Default to 'ST' (Student) if not provided
+        user_type = validated_data.pop('user_type', 'VI')  # Default to 'VI' (visitors) if not provided
         user = CustomUser.objects.create_user(**validated_data, user_type=user_type)
         return user
 
--- dreamshala/settings.py
+++ dreamshala/settings.py
@@ -16,8 +16,8 @@ import os.path
 import sys
 
 PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
